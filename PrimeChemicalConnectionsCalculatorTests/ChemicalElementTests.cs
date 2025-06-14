fc9e32f8bb81f1b1c374aa4d9cc19642f30c3e57
Sergey <mr.ultimateground@gmail.com>
2025-06-14 22:50:57 +0200

    ChemicalElement class improoved ChemicalElement unit tests

---
+++ PrimeChemicalConnectionsCalculatorTests/ChemicalElementTests.cs
@@ -0,0 +1,63 @@
+﻿using PrimeChemicalConnectionsClaculator;
+
+namespace PrimeChemicalConnectionsCalculatorTests
+{
+    [TestClass]
+    public sealed class ChemicalElementTests
+    {
+        [TestMethod]
+        public void ChemicalElementPeriodTest()
+        {
+            ChemicalElement e1 = new ChemicalElement(number: 1, formula: "H", name: "_", mass: 100, electronegativity: 1);
+            ChemicalElement e2 = new ChemicalElement(number: 25, formula: "Mn", name: "_", mass: 100, electronegativity: 1);
+            ChemicalElement e3 = new ChemicalElement(number: 55, formula: "Cs", name: "_", mass: 100, electronegativity: 1);
+            ChemicalElement e4 = new ChemicalElement(number: 64, formula: "Gd", name: "_", mass: 100, electronegativity: 1);
+            ChemicalElement e5 = new ChemicalElement(number: 118, formula: "Og", name: "_", mass: 1000, electronegativity: 1);
+            ChemicalElement e6 = new ChemicalElement(number: 2, formula: "He", name: "_", mass: 100, electronegativity: 1);
+
+            Assert.AreEqual<uint>(e1.Period, 1);
+            Assert.AreEqual<uint>(e2.Period, 4);
+            Assert.AreEqual<uint>(e3.Period, 6);
+            Assert.AreEqual<uint>(e4.Period, 6);
+            Assert.AreEqual<uint>(e5.Period, 7);
+            Assert.AreEqual<uint>(e6.Period, 1);
+
+        }
+        [TestMethod]
+        public void ChemicalElementGroupTest()
+        {
+            var e1 = new ChemicalElement(number: 1, formula: "H", name: "_", mass: 100, electronegativity: 1).Group;
+            var e2 = new ChemicalElement(number: 25, formula: "Mn", name: "_", mass: 100, electronegativity: 1).Group;
+            var e3 = new ChemicalElement(number: 55, formula: "Cs", name: "_", mass: 100, electronegativity: 1).Group;
+            var e4 = new ChemicalElement(number: 64, formula: "Gd", name: "_", mass: 100, electronegativity: 1).Group;
+            var e5 = new ChemicalElement(number: 118, formula: "Og", name: "_", mass: 1000, electronegativity: 1).Group;
+            var e6 = new ChemicalElement(number: 2, formula: "He", name: "_", mass: 100, electronegativity: 1).Group;
+
+            Assert.AreEqual<uint>(e1, 1);
+            Assert.AreEqual<uint>(e2, 2);
+            Assert.AreEqual<uint>(e3, 1);
+            Assert.AreEqual<uint>(e4, 2);
+            Assert.AreEqual<uint>(e5, 8);
+            Assert.AreEqual<uint>(e6, 8);
+
+        }
+        [TestMethod]
+        public void ChemicalElementConstructorTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new ChemicalElement(
+                number: 0, formula: "H", name: "_", mass: 1, electronegativity: 1));
+            Assert.ThrowsException<ArgumentException>(() => new ChemicalElement(
+                number: 10, formula: "H", name: "_", mass: 20, electronegativity: -1));
+            Assert.ThrowsException<ArgumentException>(() => new ChemicalElement(
+                number: 10, formula: "H", name: "_", mass: 1, electronegativity: 1));
+            try
+            {
+                new ChemicalElement(number: 1, formula: "H", name: "_", mass: 1, electronegativity: 1);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();                
+            }
+        }
+    }
+}
--- PrimeChemicalConnectionsClaculator/ChemicalElement.cs
+++ PrimeChemicalConnectionsClaculator/ChemicalElement.cs
@@ -12,8 +12,17 @@ namespace PrimeChemicalConnectionsClaculator
         private string _formula;
         private string _name;
         private uint _mass;
-        private int _charge;
-
+        private float _electronegativity;
+        private uint groupPos()
+        {
+            uint[] sizes = { 2, 8, 8, 18, 18, 32, 32 };
+            uint res = _number;
+            for (uint i = 0; i < Period-1; i++)
+            {
+                res -= sizes[i];
+            }
+            return res;
+        }
         public uint Number
         {
             get { return _number; }
@@ -22,50 +31,113 @@ namespace PrimeChemicalConnectionsClaculator
         {
             get { return _formula; }
         }
-
         public string Name
         {
             get { return _name; }
         }
-
         public uint Mass
         {
             get { return _mass; }
         }
 
-        public int Charge
+        public uint Period
         {
-            get { return _charge; }
+            get
+            {
+                if (_number == 0)
+                    throw new Exception("atomic number cannot be zero");
+                else if (_number >= 1 && _number <= 2)
+                    return 1;
+                else if (_number >= 3 && _number <= 10)
+                    return 2;
+                else if (_number >= 11 && _number <= 18)
+                    return 3;
+                else if (_number >= 19 && _number <= 36)
+                    return 4;
+                else if (_number >= 37 && _number <= 54)
+                    return 5;
+                else if (_number >= 55 && _number <= 86)
+                    return 6;
+                else if (_number >= 87 && _number <= 118)
+                    return 7;
+                else
+                    throw new Exception("unknown period");
+            }
+        }
+        public uint Group
+        {
+            get
+            {
+                if (Period == 1)
+                {
+                    if (_number == 1)
+                        return 1;
+                    else return 8;
+                }
+                else if (Period == 2|| Period ==3 )
+                    return _number-2-(Period-2)*8;
+                else if (Period == 4 || Period == 5)
+                {
+                    uint pos = groupPos();
+                    if (pos == 1)
+                    {
+                        return 1;
+                    }else if (pos <=12)
+                    {
+                        return 2;
+                    }
+                    else
+                    {
+                        return pos - 10;
+                    }
+                }
+                else if (Period == 6 || Period == 7)
+                {
+                    uint pos = groupPos();
+                    if (pos == 1)
+                    {
+                        return 1;
+                    }
+                    else if (pos <= 27)
+                    {
+                        return 2;
+                    }
+                    else
+                    {
+                        return pos - 24;
+                    }
+                }
+                throw new Exception("unknown group");
+            }
         }
 
-        public ChemicalElement(uint number, string formula, string name, uint mass, int charge)
+        public ChemicalElement(uint number, string formula, string name, uint mass, float electronegativity)
         {
-            if (mass > 0)
+            if (number > 0)
             {
-                this._number = number;
+                _number = number;
             }
             else
             {
                 throw new ArgumentException("Atomic number cannot be zero or less");
             }
-            this._formula = formula;
-            this._name = name;
-            if (mass > 0)
+            _formula = formula;
+            _name = name;
+            if (mass >= _number)
             {
-                this._mass = mass;
+                _mass = mass;
             }
             else
             {
-                throw new ArgumentException("mass cannot be zero or less");
+                throw new ArgumentException("mass cannot be less than Atomic number");
             }
-
-            if (charge >= -3 && charge <= 4)
+            if (electronegativity>0.0f && electronegativity<5.0f)
             {
-                this._charge = charge;
+                _electronegativity = electronegativity;
             }
             else
             {
-                throw new ArgumentException("charge must be in interval between -3 and 4");
+                throw new ArgumentException("Electronegativity must be between 0 and 5");
             }
 
         }
